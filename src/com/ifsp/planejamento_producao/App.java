c0e82af12dfe8175ad97317377004776b8ec0eb4
Gustavo-Stedile <gustavostedile1@hotmail.com>
2025-10-31 13:13:49 -0300

    formatando as bases do projeto

--- src/com/ifsp/planejamento_producao/App.java
+++ src/com/ifsp/planejamento_producao/App.java
@@ -1,21 +1,7 @@
 package com.ifsp.planejamento_producao;
 
-import com.ifsp.planejamento_producao.controller.AreaController;
-import com.ifsp.planejamento_producao.model.Area;
-
 public class App {
     public static void main(String[] args) {
-        try {
-            AreaController controller = new AreaController();
-            controller.insert(new Area(
-                "para plantar feijão",
-                "feijão",
-                7
-            ));
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        System.out.println("Bem Vindo à gugulândia");
     }
 }
--- src/com/ifsp/planejamento_producao/model/SQL.java
+++ src/com/ifsp/planejamento_producao/model/SQL.java
@@ -4,6 +4,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * classe em que as informações de conexão estarão guardadas!
+ */
 public class SQL {
     public static final String URL = "jdbc:mysql://localhost:3306/planejamento_producao";
     public static final String USER = "root";
@@ -12,4 +15,4 @@ public class SQL {
     public static Connection connect() throws SQLException {
         return DriverManager.getConnection(URL, USER, PSWD);
     }
-}   
+}
